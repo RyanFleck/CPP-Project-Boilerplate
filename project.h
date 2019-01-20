128161edeaf6beb12cb79c79406f84cf201c1eb7
Ryan Fleck <ryan.fleck@protonmail.com>
2019-01-20 13:30:42 -0500

    Standardize filenames, point to fsource in header.

--- project.h
+++ project.h
@@ -5,10 +5,15 @@
 
  #include <bits/stdc++.h>
 
+// arithmetic.cpp
 int add(int x, int y);
 int subtract(int x, int y);
+
+// basic_io.cpp
 int printArrayFromCIN();
 int grabIntFromCIN();
+
+// bubble_sort.cpp
 int bubbleSort(int *a, int len);
 int printArray(int *a, int len);
 
